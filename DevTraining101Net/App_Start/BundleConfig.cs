c330e9b972ccca5f304b4a5e914d9acf43694e99
Mike Borozdin <mikeb@docusign.com>
2014-03-24 07:50:33 -0700

    first public push

---
+++ DevTraining101Net/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+
+namespace DevTraining101Net.App_Start
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/style.css"));
+        }
+    }
+}
---
+++ DevTraining101Net/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DevTraining101Net.MvcApplication" Language="C#" %>
